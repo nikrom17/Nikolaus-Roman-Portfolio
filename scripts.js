4ede3e555df35062ce9c3e544309579fe92927d0
Nik <nikrom17@gmail.com>
2019-03-22 15:13:25 -0500

    first commit of portfolio project

---
+++ scripts.js
@@ -0,0 +1,50 @@
+// let scrollCount = 0;
+
+// function preventDefault(e) {
+//       e.preventDefault();
+//       e.returnValue = false;
+// }
+
+// var lastScrollTop = 0;
+// window.addEventListener("scroll", function(e) {
+//     if (scrollCount > 20) {
+//         console.log('fuck');
+//         preventDefault(e);
+//         window.scrollBy(0, 500);
+//         scrollCount = 0;
+//     } else {
+//         scrollCount++;
+//         preventDefault(e);
+//         console.log('you');
+//     }
+// }, false);
+
+
+
+// // window.addEventListener('scroll', function scroll(e) {
+// //   scrollCount++;
+// //   console.log(e);
+// //   if (scrollCount > 20) {
+// //     window.scrollBy(0, 100); // Scroll 100px downwards
+// //     scrollCount = 0;
+// //   }
+// //   window.onwheel = preventDefault; // modern standard
+// //   window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+// //   window.ontouchmove  = preventDefault; // mobile
+// //   document.onkeydown  = preventDefaultForScrollKeys;
+// // });
+
+// let keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+
+// function preventDefaultForScrollKeys(e) {
+//     if (keys[e.keyCode]) {
+//         preventDefault(e);
+//         return false;
+//     }
+// }
+
+// function scrollWin(e) {
+//     console.log(e)
+//   }
+
